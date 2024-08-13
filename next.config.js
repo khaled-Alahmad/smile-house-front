15e7ba081f10a0f7a9bf03eabe72186f86b1fb1f
khaled alahmad <eng.khaledalahmad@gmail.com>
2024-08-13 17:50:07 +0300

    upadate

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
   },
 };
 module.exports = {
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      });
+    }
+    return headers;
+  },
   async redirects() {
     return [
       {
