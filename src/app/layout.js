305336bafe7814495c2896dae9466c7c7961d616
khaled-alahmad <eng.khaledalahmad@gmail.com>
2024-10-30 12:28:13 +0300

    "Updated meta tags, added Twitter and schema.org metadata, and rearranged layout.js code"

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,33 +27,51 @@ export default function RootLayout({ children }) {
           name="description"
           content="مركز سمايل هاوس في إدلب يقدم أفضل خدمات تجميل الأسنان والعناية بالبشرة وغيرها من الخدمات التجميلية."
         />
-        {/* <meta property="og:title" content="سمايل هاوس - مركز تجميل في إدلب" /> */}
-        <meta property="title" content="سمايل هاوس - مركز تجميل في إدلب" />
 
-        {/* <meta
+        <meta property="og:title" content="سمايل هاوس - مركز تجميل في إدلب" />
+        <meta
           property="og:description"
           content="احصل على أجود خدمات التجميل في سمايل هاوس بإدلب، من تجميل الأسنان إلى العناية بالبشرة."
-        /> */}
+        />
+        <meta
+          property="og:image"
+          content="https://smile-house.vercel.app/images/logo.png"
+        />
+        <meta property="og:url" content="https://smile-house.vercel.app" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="سمايل هاوس - مركز تجميل في إدلب" />
+        <meta
+          name="twitter:description"
+          content="مركز سمايل هاوس في إدلب يقدم أفضل خدمات تجميل الأسنان والعناية بالبشرة."
+        />
+        <meta
+          name="twitter:image"
+          content="https://smile-house.vercel.app/images/logo.png"
+        />
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "سمايل هاوس",
+            "url": "https://smile-house.vercel.app",
+            "logo": "https://smile-house.vercel.app/images/logo.png",
+            "sameAs": [
+              "https://www.facebook.com/p/%D9%85%D8%AC%D9%85%D8%B9-%D8%B3%D9%85%D8%A7%D9%8A%D9%84-%D9%87%D8%A7%D9%88%D8%B3-%D8%A7%D9%84%D8%B7%D8%A8%D9%8A-%D9%80-Smile-House-100092169591579/",
+              "https://www.instagram.com/smile_house11/?hl=ar"
+            ]
+          }
+        `}
+        </script>
+
         <meta
           name="google-site-verification"
           content="Miu_QGaGP4B9u6IllJxprdJbtujQfQUFvpyxxnJZy0Q"
         />
-        <link
-          rel="icon"
-          href="https://smile-house.vercel.app/favicon.ico"
-          type="image/x-icon"
-        />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
-        {/* <meta
-          property="og:image"
-          content="https://smile-house.vercel.app/images/logo.png"
-        /> */}
-        <meta
-          property="image"
-          content="https://smile-house.vercel.app/images/logo.png"
-        />
-        <meta property="url" content="https://smile-house.vercel.app" />
-        {/* <meta property="og:url" content="https://smile-house.vercel.app" /> */}
+        <meta name="robots" content="index, follow" />
 
         <meta name="robots" content="index, follow" />
         <link
@@ -80,7 +98,7 @@ export default function RootLayout({ children }) {
         />
 
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
