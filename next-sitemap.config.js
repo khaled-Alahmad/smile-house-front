9779e914d1f2a7a5d1c43a9a349802c07f7becf5
khaled-alahmad <eng.khaledalahmad@gmail.com>
2024-10-30 11:53:00 +0300

    "Updated next-sitemap.config.js to include additionalPaths for SSR routes and added new routes to sitemap-0.xml"

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,23 @@
+const fetchSSRRoutes = async () => {
+  // Add logic here to retrieve paths for your SSR routes,
+  // e.g., fetching IDs or slugs from an API or database
+  const routes = [{ loc: "/department" }, { loc: "/services-details/16" }]; // example routes
+  return routes;
+};
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://smile-house.vercel.app',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: "https://smile-house.vercel.app",
+  generateRobotsTxt: true,
+};
+
+module.exports = {
+  siteUrl: "https://smile-house.vercel.app",
+  generateRobotsTxt: true,
+  additionalPaths: async (config) => {
+    const ssrRoutes = await fetchSSRRoutes();
+    return ssrRoutes.map((route) => ({
+      loc: route.loc,
+      lastmod: new Date().toISOString(),
+    }));
+  },
+};
